4cf0ef0b3e43d85443f1c3779e1dde94630ed6ab
Nino Chogovadze <135849986+nichogovadze@users.noreply.github.com>
2024-05-18 23:05:31 +0400

    First Commit

---
+++ ServiceConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using MongoDB.Bson.IO;
+using System.Globalization;
+using System.Reflection;
+using System;
+using MongoDbCrudApi.Services;
+using System.Configuration;
+using MongoDB.Driver;
+
+namespace MongoDbCrudApi
+{
+    public static class ServiceConfiguration
+    {
+        public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder builder)
+        {
+            var mongoConfig = builder.Configuration.GetSection(nameof(MongoDbSettings));
+            builder.Services.Configure<MongoDbSettings>(mongoConfig);
+            
+            builder.Services.AddSingleton<ItemService>();
+
+            builder.Services.AddControllers();
+
+            return builder;
+        }
+    }
+
+    public class MongoDbSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
