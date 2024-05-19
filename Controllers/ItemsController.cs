7cf0563aa35850b1192d4d9c9447c20edd72147d
Nino Chogovadze <NiChogovadze@tbcbank.com.ge>
2024-05-19 18:13:24 +0400

    add files

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDbCrudApi.Models;
+using MongoDbCrudApi.Services;
+
+namespace MongoDbCrudApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private readonly ItemService _itemService;
+
+        public ItemsController(ItemService itemService)
+        {
+            _itemService = itemService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Item>> Get() => _itemService.Get();
+
+        [HttpGet("{id}", Name = "GetItem")]
+        public ActionResult<Item> Get(string id)
+        {
+            var item = _itemService.Get(id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
+
+        [HttpPost]
+        public ActionResult<Item> Create(Item item)
+        {
+            _itemService.Create(item);
+
+            return CreatedAtRoute("GetItem", new { id = item.Id.ToString() }, item);
+        }
+
+        [HttpPut]
+        public IActionResult Update(string id, Item itemIn)
+        {
+            var item = _itemService.Get(id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            _itemService.Update(id, itemIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(string id)
+        {
+            var item = _itemService.Get(id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            _itemService.Remove(item.Id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ Services/ItemService.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Driver;
+using MongoDbCrudApi.Models;
+
+namespace MongoDbCrudApi.Services
+{
+    public class ItemService
+    {
+        private readonly IMongoCollection<Item> _items;
+
+        public ItemService(IConfiguration config)
+        {
+            var connectionString = config.GetSection("MongoDbSettings:ConnectionString").Value;
+            var client = new MongoClient(connectionString);
+            var database = client.GetDatabase("ItemDb");
+            _items = database.GetCollection<Item>("Items");
+        }
+
+        public List<Item> Get() => _items.Find(item => true).ToList();
+
+        public Item Get(string id) => _items.Find<Item>(item => item.Id == id).FirstOrDefault();
+
+        public Item Create(Item item)
+        {
+            _items.InsertOne(item);
+            return item;
+        }
+
+        public void Update(string id, Item itemIn) => _items.ReplaceOne(item => item.Id == id, itemIn);
+
+        public void Remove(Item itemIn) => _items.DeleteOne(item => item.Id == itemIn.Id);
+
+        public void Remove(string id) => _items.DeleteOne(item => item.Id == id);
+    }
+}
